62ad51f7fb26a1413322c36cea79eb035f475c5c
ccnulyq <ccnulyq@163.com>
2019-03-20 16:06:34 +0800

    在github上修改代码，模拟有同事提交了新代码

--- Git-demo-01-start/src/cn/gzsxt/git/test/TestGit.java
+++ Git-demo-01-start/src/cn/gzsxt/git/test/TestGit.java
@@ -5,4 +5,7 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("hello,git");
 	}
+	public void testGit(){
+	        System.out.println("hello,testGit");
+	}
 }
