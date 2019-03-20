f360ebc4b9b653c631199ab01c296948d1268086
ccnulyq <ccnulyq@163.com>
2019-03-20 16:21:51 +0800

    在github上编辑代码，模拟代码冲突

--- Git-demo-01-start/src/cn/gzsxt/git/test/TestGit.java
+++ Git-demo-01-start/src/cn/gzsxt/git/test/TestGit.java
@@ -4,6 +4,7 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		System.out.println("hello,git");
+		int a = 2;
 	}
 	public void testGit(){
 	        System.out.println("hello,testGit");
